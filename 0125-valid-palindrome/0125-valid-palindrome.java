aac1fddeb5b4deff28ec64e0cf815734bce55072
ANKIT ANAND <112458145+AnkitAnandaq@users.noreply.github.com>
2024-10-17 10:53:07 +0530

    Time: 13 ms (40.69%), Space: 44.6 MB (39.16%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,12 +1,24 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder trimmedStr = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            if (Character.isLetterOrDigit(c)) {
-                trimmedStr.append(Character.toLowerCase(c));
-            }}
-        String forwardStr = trimmedStr.toString();
-        String reversedStr = trimmedStr.reverse().toString();
-        return forwardStr.equals(reversedStr);
+
+         String result = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+         int start=0;
+         int end=result.length()-1;
+         while(start<=end){
+            if(result.charAt(start)!=result.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+         }
+         return true;
+        // StringBuilder trimmedStr = new StringBuilder();
+        // for (char c : s.toCharArray()) {
+        //     if (Character.isLetterOrDigit(c)) {
+        //         trimmedStr.append(Character.toLowerCase(c));
+        //     }}
+        // String forwardStr = trimmedStr.toString();
+        // String reversedStr = trimmedStr.reverse().toString();
+        // return forwardStr.equals(reversedStr);
     }
 }
